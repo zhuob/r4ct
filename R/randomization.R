5a1914b57b15df9e55e0183f5527a6692dabf09a
Zhuo <bzhuo@amgen.com>
2019-06-04 22:26:55 -0700

    change the structure of rand_arm

--- R/randomization.R
+++ R/randomization.R
@@ -1,31 +1,31 @@
 
 #' @title generate blocked randomized arms
 #'
-#' @param nsbj a vector specifying number of subjects to be randominzed for each
-#'   arm
+#' @param nsbj an integer for total number of subjects to be randominzed
 #' @param ratio the allocation ratio
+#' @param arm_name a vector of characters for arms
 #'
 #' @return a vector of length `sum(nsbj)` with randomized treatment arms
 #' @export
 #'
 #' @examples
-#' rand_arm(nsbj = c(0, 1), ratio = c(1, 1))
-#' rand_arm(nsbj = c(5, 5, 2), ratio = c(2, 2, 1))
+#' rand_arm(nsbj = 1, ratio = c(1, 1))
+#' rand_arm(nsbj = 12, ratio = c(2, 2, 1))
 #'
-rand_arm <- function(nsbj, ratio){
+rand_arm <- function(nsbj, ratio, arm_name = paste("arm", 1:length(ratio), sep = "_")){
 
-  n_b <- nsbj / ratio;
-  n_block <- floor(min(n_b))
-  n_remainder <- nsbj - n_block*ratio
-  n_arm <- length(nsbj)
-  arm_name <- rep(paste("arm", 1:n_arm, sep = "_"), ratio)
+  n_arm <- length(ratio)
+  n_block <- floor(nsbj / sum(ratio))
+  n_per_arm <- rep(n_block, n_arm)
+  n_remainder <- nsbj - sum(n_per_arm * ratio)
+  arms <- rep(arm_name, ratio)
+
   if(n_block == 0){
     arm_1 <- NULL
   } else{
-  arm_1 <- as.vector(replicate(n_block, sample(arm_name)))
+  arm_1 <- as.vector(replicate(n_block, sample(arms)))
   }
-  arm_2 <- rep(paste("arm", 1:n_arm, sep = "_"), n_remainder)
-  arm_2 <- sample(arm_2) ## shuffle it
+  arm_2 <- sample(arm_name, n_remainder)
   arm <- c(arm_1, arm_2)
   return(arm)
 }
