61d096dbfc5c071bebead7cdeabac5e75e7739fb
Bin Zhuo <rickyb.zh@gmail.com>
2021-12-20 17:31:36 +0800

    Increment version number to 0.2.2

--- inst/shiny-examples/bayes-go-nogo/shiny_module_01.R
+++ inst/shiny-examples/bayes-go-nogo/shiny_module_01.R
@@ -92,7 +92,14 @@ module_01$comp_01$ui <- function(){
                                     tabPanel("Go/NoGo Probabilities by Interim", 
                                              plotOutput("plot2")), 
                                     tabPanel("Overall Operating Characteristics", 
-                                             tableOutput("table2")))
+                                             tableOutput("table2"), 
+                                             tags$br(), 
+                                             
+                                             h3("Download Simulation Report"), 
+                                             downloadButton(outputId = "gng_report", class = "bright", icon = icon("cloud-download-alt")), 
+                                             radioButtons(inputId = "report_format", label = "Choose a Format", choices = c("HTML", "PDF", "WORD"), selected = "HTML")
+                                          
+                                             ))
                       )
              )
   )
@@ -238,6 +245,33 @@ module_01$comp_01$server <- function(input, output, session, data){
       readr::write_csv(psim2, file)
     }
   )
+  
+  output$gng_report <- downloadHandler(
+    filename = function(){
+      paste("gng-report", sep = ".", 
+            switch(input$report_format, PDF = "pdf", HTML = "html", WORD = "docx")
+        )
+    }, 
+    
+    content = function(file){
+      src <- normalizePath("gng-report.Rmd")
+      
+      out_dir <- setwd(tempdir())
+      on.exit(setwd(out_dir))
+      file.copy(src, "gng-report.Rmd", overwrite = TRUE)
+      
+      out <- rmarkdown::render("gng-report.Rmd", 
+                               switch(input$report_format, 
+                                      PDF = rmarkdown::pdf_document(), 
+                                      HTML = rmarkdown::html_document(), 
+                                      WORD = rmarkdown::word_document()))
+      
+      file.rename(out, file)
+    }
+  )
+  
+  
+  
   
 }
 
